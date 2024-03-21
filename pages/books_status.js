2e10c9e12740db5efe0f2ae3ae0f8e6126da30ef
Parthiv-M <parthivmenon.dev@gmail.com>
2024-03-21 14:49:38 -0400

    Changes for author and book status routes

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,7 @@
 let BookInstance = require('../models/bookinstance');
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
+exports.show_all_books_status = async function(res) {
+  const books = await BookInstance.find({ status: "Available" }).populate("book");
+  let bookInstances = books.map((book) => book.book.title + " - " + book.status)
+  return res.send(bookInstances);
 }
